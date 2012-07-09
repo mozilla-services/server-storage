e4e7be2573ae9c6037644f32d022f63c73930cc8
Ryan Kelly <rfkelly@mozilla.com>
2012-07-10 09:22:51 +1000

    Bug 764065 - port grinder loadtest script to funkload; r=petef

---
+++ loadtest/dist.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+workers="client4.scl2.svc.mozilla.com client5.scl2.svc.mozilla.com client6.scl2.svc.mozilla.com client7.scl2.svc.mozilla.com client8.scl2.svc.mozilla.com client9.scl2.svc.mozilla.com"
+dest_dir="$HOME/syncstorage-loadtest"
+source_dir=$(dirname $0)
+
+trap "echo '==> killing bench runs'; xapply 'ssh %1 killall fl-run-bench' $workers" EXIT
+
+echo "==> killing existing bench runs"
+xapply "ssh %1 killall fl-run-bench" $workers
+
+echo "==> syncing files to workers"
+xapply "rsync $source_dir/{StressTest.conf,stress.py,Makefile} %1:$dest_dir/" $workers
+
+echo "==> building virtualenvs"
+xapply "ssh %1 cd $dest_dir \; rm loadtest*\; make build" $workers
+
+echo "==> running load"
+while :; do
+    xapply -xP10 "ssh %1 cd $dest_dir \; make bench" $workers
+done
--- loadtest/stress.py
+++ loadtest/stress.py
@@ -39,51 +39,55 @@ Load test for the Storage server
 import os
 import base64
 import random
+import socket
 import json
 import time
 
 from funkload.FunkLoadTestCase import FunkLoadTestCase
 from funkload.utils import Data
 
-HOST = 'http://localhost:5000'
 VERSION = '1.1'
 collections = ['bookmarks', 'forms', 'passwords', 'history', 'prefs', 'tabs']
-
+get_count_distribution = [0, 71, 15, 7, 4, 3] # 0% 0 GETs, 71% 1 GET, etc.
+post_count_distribution = [67, 18, 9, 4, 2]   # 67% 0 POSTs, 18% 1 POST, etc.
 
 class StressTest(FunkLoadTestCase):
 
     def setUp(self):
-        self.root = 'http://localhost'
+        pass
 
-    def test_get_post(self):
-        username = "ziafhkdmaey2s6v5beidpzenllgj6276"
-        password = "passat76"
+    def test_storage_session(self):
+        username = self._pick_user()
+        password = "password"
+        node = self._pick_node()
+        self.logd("choosing node %s" % (node))
         self.setBasicAuth(username, password)
 
         # GET /username/info/collections
-        url = "/%s/%s/info/collections" % (VERSION, username)
-        url = self.root + url
+        self.setOkCodes([200, 404])
+        url = node + "/%s/%s/info/collections" % (VERSION, username)
         response = self.get(url)
-        self.assertEqual(response.code, 200)
 
-        # GET /username/storage/collection
-        collection = random.choice(collections)
-        url = "/%s/%s/storage/%s?full=1" % (VERSION, username, collection)
-        url = self.root + url
-        response = self.get(url)
-        self.assertEqual(response.code, 200)
-        body = response.body
-        self.assertTrue(body != '')
+        c = collections[:]
+        random.shuffle(c)
 
-        # POST /username/storage/collection
-        collection = random.choice(collections)
-        payload = 'a' * 1000
-        url = "/%s/%s/storage/%s" % (VERSION, username, collection)
-        url = self.root + url
+        # GET requests
+        self.setOkCodes([200, 404])
+        # we subtract 1 because we already did a GET on info/collections
+        for x in range(self._pick_weighted_count(get_count_distribution) - 1):
+            url = node + "/%s/%s/storage/%s" % \
+                  (VERSION, username, collections[x])
+            newer = int(time.time() - random.randint(3600, 360000))
+            params = {"full": "1", "newer": str(newer)}
+            self.logd("about to GET (x=%d) %s" % (x, url));
+            response = self.get(url, params)
 
-        # we want to put a batch of 100, 4 times
-        # like FF does
-        for x in range(4):
+        # PUT requests with 100 WBOs batched together
+        self.setOkCodes([200])
+        for x in range(self._pick_weighted_count(post_count_distribution)):
+            url = node + "/%s/%s/storage/%s" % \
+                  (VERSION, username, collections[x])
+            payload = username * random.randint(50, 200)
             data = []
             for i in range(100):
                 id = base64.b64encode(os.urandom(10))
@@ -92,7 +96,29 @@ class StressTest(FunkLoadTestCase):
                 data.append(wbo)
             data = json.dumps(data)
             data = Data('application/json', data)
+            self.logd("about to POST (x=%d) %s" % (x, url));
             response = self.post(url, params=data)
-            self.assertEqual(response.code, 200)
             body = response.body
             self.assertTrue(body != '')
+
+    def _pick_node(self):
+        node = None
+        # 41 <= node <= 50 sync5.db.scl2.stage down
+        while node is None or (node >= 41 and node <= 50):
+            node = random.randint(1, 80)
+        return "https://stage-sync%i.services.mozilla.com" % node
+
+    def _pick_user(self):
+        return "cuser%i" % random.randint(1, 1000000)
+
+    def _pick_weighted_count(self, weights):
+        i = random.randint(1, sum(weights))
+        count = 0
+        base = 0
+        for weight in weights:
+            base += weight
+            if i <= base:
+                break
+            count += 1
+
+        return count
